f94684f39bfaf90300f9c691b4537fc944f13cdc
rishitaSharma999 <rishita2004sharma@gmail.com>
2025-04-05 01:26:14 +0530

    second

--- app.py
+++ app.py
@@ -155,5 +155,6 @@ def predict():
     
     return jsonify({'error': 'Invalid file type'})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)  # Change port as needed
+
